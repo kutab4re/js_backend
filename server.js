78da2f150075b37fe8ab43b496b65fa49122e0e5
Alex <alfa_no_1@list.com>
2025-06-16 00:06:40 +0300

    Compleat server

--- server.js
+++ server.js
@@ -5,60 +5,65 @@ const app = express();
 const port = 8000;
 
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('public')); 
 
 async function getDbCollection(dbAddress, dbName,dbCollectionName) {
 	const client = new MongoClient(dbAddress);
-    await client.connect();
-    const db = client.db(dbName);
-    return db.collection(dbCollectionName);
+    await client.connect();
+    const db = client.db(dbName);
+    return db.collection(dbCollectionName);
 }
 
 
-app.get('/tasks', async function(req, res) {
-    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
-    const data = await collection.find({}).toArray();
-    res.send(data);
+
+app.get('/swords', async function(req, res) {
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'swordinventory', 'swords'); 
+    const data = await collection.find({}).toArray();
+    res.send(data);
 });
 
-app.get('/tasks/:id', async function(req, res) {
-    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
-    const data = await collection.findOne({_id: new ObjectId(req.params.id)});
-    res.send(data);
+
+app.get('/swords/:id', async function(req, res) {
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'swordinventory', 'swords'); 
+    const data = await collection.findOne({_id: new ObjectId(req.params.id)});
+    res.send(data);
 });
 
-app.post('/tasks', async function(req, res) {
-	const task = {...req.body, done: false}
-    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
-    await collection.insertOne(task);
-    res.send(task);
+
+app.post('/swords', async function(req, res) {
+	const sword = {...req.body, available: true} 
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'swordinventory', 'swords'); 
+    await collection.insertOne(sword);
+    res.send(sword);
 });
 
-app.patch('/tasks/:id', async function(req, res){
-    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
-    const taskId = req.params.id;
-    const updateData = req.body;
+app.patch('/swords/:id', async function(req, res){
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'swordinventory', 'swords'); 
+    const swordId = req.params.id; 
+    const updateData = req.body;
 
-    try {
-        const result = await collection.updateOne(
-            { _id: new ObjectId(taskId) },
-            { '$set': updateData }
-        );
-        if (result.matchedCount === 1) {
-            res.status(200).send({});
-        } else {
-            res.status(404).send({ message: 'Task not found' });
-        }
-    } catch (error) {
-        res.status(500).send({ message: 'Internal server error' });
-    }
+    try {
+        const result = await collection.updateOne(
+            { _id: new ObjectId(swordId) }, 
+            { '$set': updateData }
+        );
+        if (result.matchedCount === 1) {
+            res.status(200).send({});
+        } else {
+            res.status(404).send({ message: 'Sword not found' }); 
+        }
+    } catch (error) {
+        res.status(500).send({ message: 'Internal server error' });
+    }
 });
-app.delete('/tasks/:id', async function(req, res) {
-    const collection = await getDbCollection('mongodb://127.0.0.1', 'todoapp', 'tasks');
-    await collection.deleteOne({_id: new ObjectId(req.params.id)});
-    res.send({});
+
+
+app.delete('/swords/:id', async function(req, res) {
+    const collection = await getDbCollection('mongodb://127.0.0.1', 'swordinventory', 'swords'); 
+    await collection.deleteOne({_id: new ObjectId(req.params.id)});
+    res.send({});
 });
 
 app.listen(port, function() {
-    console.log('Server is started!');
+    console.log('Server for Sword Inventory is started on port ' + port + '!'); 
 });
